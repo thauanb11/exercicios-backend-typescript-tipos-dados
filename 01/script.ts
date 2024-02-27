e63aa0ee15b482bed824febeb944e56a224b8707
thauanb11 <guedesbruno40@gmail.com>
2024-02-27 06:00:17 -0300

    implementação do typecript

---
+++ 01/script.ts
@@ -0,0 +1,41 @@
+const usuarios: { nome: string, pets: string[] }[] = [
+    {
+        nome: "João",
+        pets: ["Max"],
+    },
+    {
+        nome: "Ana",
+        pets: ["Pingo", "Lulu"],
+    },
+    {
+        nome: "Beatriz",
+        pets: ["Lessie"],
+    },
+    {
+        nome: "Carlos",
+        pets: ["Farofa", "Salsicha", "Batata"],
+    },
+    {
+        nome: "Antonio",
+        pets: ["Naninha"],
+    },
+]
+
+const buscarDonoPet = (lista: { nome: string, pets: string[] }[], nomePet: string): void => {
+    let usuarioEncontrado: { nome: string; pets: string[] } | undefined
+
+    for (const usuario of lista) {
+        if (usuario.pets.includes(nomePet)) {
+            usuarioEncontrado = usuario;
+        }
+    }
+
+    if (usuarioEncontrado) {
+        console.log(`O dono(a) do(a) ${nomePet} é o(a) ${usuarioEncontrado.nome}`);
+    } else {
+        console.log(`Que pena ${nomePet}, não encontramos seu dono(a)`);
+    }
+}
+
+
+buscarDonoPet(usuarios, 'Lulu');
